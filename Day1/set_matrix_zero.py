4fe95ae768ce74f9b66152193c785cd04b473cf3
Utkarsh Mhatre <126488995+5N0WDEN@users.noreply.github.com>
2025-08-16 01:24:55 +0530

    Create set_matrix_zero.py

---
+++ Day1/set_matrix_zero.py
@@ -0,0 +1,33 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        row_flag, col_flag = False, False
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                col_flag = True
+        for j in range(len(matrix[0])):
+            if matrix[0][j] == 0:
+                row_flag = True
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0], matrix[0][j] = 0, 0
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == 0:
+                for j in range(1, len(matrix[0])):
+                    matrix[i][j] = 0
+        for j in range(1, len(matrix[0])):
+            if matrix[0][j] == 0:
+                for i in range(1, len(matrix)):
+                    matrix[i][j] = 0
+        if row_flag and col_flag:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        elif row_flag:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        elif col_flag:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        return matrix
